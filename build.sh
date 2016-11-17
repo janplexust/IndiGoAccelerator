6593b357ce637bac0acba125e98f93b3fb23b2fc
janplexust <janp@plexust.xyz>
2016-11-17 15:52:00 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /opt/IndiGoAccelerator
-git pull
-rm -rf /opt/bitnami/opencart/catalog/view/theme/default/*
-cp /opt/IndiGoAccelerator/* /opt/bitnami/opencart/catalog/view/theme/default/ -R
